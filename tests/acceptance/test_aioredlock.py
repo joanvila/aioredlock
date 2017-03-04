8e1946cd52143d3ed862452abdff6a3d9d96bee5
joanvila <joan.vila@skyscanner.net>
2017-03-04 11:56:25 +0100

    Changes in acceptance test. Not passing yet

--- tests/acceptance/test_aioredlock.py
+++ tests/acceptance/test_aioredlock.py
@@ -1,17 +1,16 @@
 import pytest
+import uuid
 from aioredlock import Aioredlock
 
 
 class TestAioredlock:
 
-    RESOURCE1 = "1"
-    RESOURCE2 = "2"
-
     @pytest.mark.asyncio
     async def test_simple_aioredlock(self, single_redis_connection):
+        resource = str(uuid.uuid4())
         lock_manager = Aioredlock(single_redis_connection)
 
-        lock = await lock_manager.lock(self.RESOURCE1)
+        lock = await lock_manager.lock(resource)
         assert lock.valid is True
 
         await lock_manager.unlock(lock)
@@ -21,12 +20,14 @@ class TestAioredlock:
 
     @pytest.mark.asyncio
     async def test_aioredlock_two_locks_on_different_resources(self, single_redis_connection):
+        resource1 = str(uuid.uuid4())
+        resource2 = str(uuid.uuid4())
         lock_manager = Aioredlock(single_redis_connection)
 
-        lock1 = await lock_manager.lock(self.RESOURCE1)
+        lock1 = await lock_manager.lock(resource1)
         assert lock1.valid is True
 
-        lock2 = await lock_manager.lock(self.RESOURCE2)
+        lock2 = await lock_manager.lock(resource2)
         assert lock2.valid is True
 
         await lock_manager.unlock(lock1)
@@ -38,12 +39,13 @@ class TestAioredlock:
 
     @pytest.mark.asyncio
     async def test_aioredlock_two_locks_on_same_resource(self, single_redis_connection):
+        resource = str(uuid.uuid4())
         lock_manager = Aioredlock(single_redis_connection)
 
-        lock1 = await lock_manager.lock(self.RESOURCE1)
+        lock1 = await lock_manager.lock(resource)
         assert lock1.valid is True
 
-        lock2 = await lock_manager.lock(self.RESOURCE1)
+        lock2 = await lock_manager.lock(resource)
         assert lock2.valid is False
 
         await lock_manager.unlock(lock1)
