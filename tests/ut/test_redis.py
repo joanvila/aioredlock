4fcad8a45d3fc77962b6997127ae074f3cb1e968
Ildar Akhmetgaleev <akhilman@gmail.com>
2018-01-03 04:33:05 +0000

    Allow to set db=N and password to redis client
    Connections list now accepts two more optional keys:
    db and password.
    All keys is potional even port can be omited.
    
    Default values is:
        host='localhost'
        port=6379
        db=0
        password=None
    
    Fixes #9

--- tests/ut/test_redis.py
+++ tests/ut/test_redis.py
@@ -53,7 +53,10 @@ class TestInstance:
             assert instance._pool is None
             pool = await instance.connect()
 
-            create_pool.assert_called_once_with(('localhost', 6379), minsize=1, maxsize=100)
+            create_pool.assert_called_once_with(
+                ('localhost', 6379),
+                db=0, password=None,
+                minsize=1, maxsize=100)
             assert pool is fake_pool
             assert instance._pool is fake_pool
 
@@ -118,8 +121,8 @@ class TestRedis:
             redis = Redis(redis_two_connections, 10)
 
             calls = [
-                call('localhost', 6379),
-                call('127.0.0.1', 6378)
+                call(host='localhost', port=6379),
+                call(host='127.0.0.1', port=6378)
             ]
             mock_instance.assert_has_calls(calls)
             assert len(redis.instances) == 2
