8ee37ddd3acefff7375c5e1044fb90f7f87a855f
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-01-04 15:35:42 +0700

    Move fixtures to conftest.py

---
+++ tests/ut/conftest.py
@@ -0,0 +1,28 @@
+import asynctest
+import pytest
+from asynctest import CoroutineMock, patch
+
+from aioredlock import Aioredlock, Lock
+
+
+async def dummy_sleep(seconds):
+    pass
+
+
+@pytest.fixture
+def locked_lock():
+    return Lock(None, "resource_name", 1, -1, True)
+
+
+@pytest.fixture
+def lock_manager_redis_patched():
+    with asynctest.patch("aioredlock.algorithm.Redis", CoroutineMock) as mock_redis:
+        with patch("asyncio.sleep", dummy_sleep):
+            mock_redis.set_lock = CoroutineMock(return_value=0.005)
+            mock_redis.unset_lock = CoroutineMock(return_value=0.005)
+            mock_redis.is_locked = CoroutineMock(return_value=False)
+            mock_redis.clear_connections = CoroutineMock()
+
+            lock_manager = Aioredlock(internal_lock_timeout=1.0)
+
+            yield lock_manager, mock_redis
--- tests/ut/test_algorithm.py
+++ tests/ut/test_algorithm.py
@@ -7,30 +7,7 @@ from asynctest import CoroutineMock, patch
 
 from aioredlock import Aioredlock, Lock, LockError
 
-
-async def dummy_sleep(seconds):
-    pass
-
 real_sleep = asyncio.sleep
-
-
-@pytest.fixture
-def locked_lock():
-    return Lock(None, "resource_name", 1, -1, True)
-
-
-@pytest.fixture
-def lock_manager_redis_patched():
-    with asynctest.patch("aioredlock.algorithm.Redis", CoroutineMock) as mock_redis:
-        with patch("asyncio.sleep", dummy_sleep):
-            mock_redis.set_lock = CoroutineMock(return_value=0.005)
-            mock_redis.unset_lock = CoroutineMock(return_value=0.005)
-            mock_redis.is_locked = CoroutineMock(return_value=False)
-            mock_redis.clear_connections = CoroutineMock()
-
-            lock_manager = Aioredlock(internal_lock_timeout=1.0)
-
-            yield lock_manager, mock_redis
 
 
 @pytest.mark.parametrize('method,exc_message', [
