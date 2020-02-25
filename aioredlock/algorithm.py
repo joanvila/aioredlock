d6c2e926f711ad4afe5c4d451227bdd59d680d49
Ildar Akhmetgaleev <akhilman@gmail.com>
2020-02-25 10:07:43 +0700

    Remove unused variable

--- aioredlock/algorithm.py
+++ aioredlock/algorithm.py
@@ -108,7 +108,7 @@ class Aioredlock:
                 # break never reached
                 raise error
 
-        except Exception as exc:
+        except Exception:
             # cleanup in case of fault or cancellation will run in background
             async def cleanup():
                 self.log.debug('Cleaning up lock "%s"', resource)
