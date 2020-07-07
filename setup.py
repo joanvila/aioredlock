62393e181bd3e9667495922e18d7df66730b4098
Daniel Wallace <danielwallace@gtmanfred.com>
2020-07-07 14:11:06 -0500

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='aioredlock',
 
-    version='0.5.0',
+    version='0.5.1',
 
     description='Asyncio implemetation of Redis distributed locks',
     long_description=long_description,
