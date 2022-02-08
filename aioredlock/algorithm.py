3e2156c6a1b9000888e11de2e6721b55d3520d4d
vasili-ii <vasili.sidorov@gmail.com>
2022-02-08 09:23:27 +0300

    prevent memory leaks in unlock def
    Added self._locks resource item cleaning into Aioredlock object unlock method to prevent memory leaks

--- aioredlock/algorithm.py
+++ aioredlock/algorithm.py
@@ -199,6 +199,7 @@ class Aioredlock:
                     self.log.exception('Can not unlock "%s"', lock.resource)
 
             self._watchdogs.pop(lock.resource)
+            self._locks.pop(lock.resource, None)
 
         await self.redis.unset_lock(lock.resource, lock.id)
         # raises LockError if can not unlock
